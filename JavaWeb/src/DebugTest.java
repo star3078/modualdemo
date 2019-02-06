58b6b76652bf5b53c04a3f7b8cf510b516018601
star3078 <star3078@gmail.com>
2019-02-06 23:36:31 +0800

    添加调试类

---
+++ JavaWeb/src/DebugTest.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+/**
+ * @author shkstart
+ * @date 2019/2/6 23:25
+ */
+public class DebugTest {
+    public static void main(String[] args) {
+
+        HashMap<String,String> map=new HashMap<>();
+        map.put("name","tom");
+        map.put("age","12");
+        map.put("school","Tsihua");
+        map.put("major","customer");
+
+        System.out.println(map);
+
+    }
+}
